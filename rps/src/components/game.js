6aee588e1b815d4089b0e35d6d06db3b982770a5
Jasmine Terry <jasmine-terry91@gmail.com>
2020-10-21 14:53:04 -0400

    file structure, brief notes

---
+++ rps/src/components/game.js
@@ -0,0 +1,2 @@
+///game functionality here
+///base logic something along lines of user pick 1 of 3...cpu does random pick--- rock beats scissor, lose to paper ---paper beat rock, lost to scissor ---scissor beat paper, lost to rock ----add point, store points in local storage
