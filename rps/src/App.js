1c942c8e855c51ff971e346dab6c7e2cabbab076
Jasmine Terry <jasmine-terry91@gmail.com>
2020-10-22 22:16:29 -0400

    images for icons added, rules svg added, basic file scaffold/setup

--- rps/src/App.js
+++ rps/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Dash from './components/dashboard';
+import {Route, Link, Switch} from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <h1> Rock, Paper, Scissors!</h1>
-        <button> Play! </button>
       </header>
+      <>
+      <Switch>
+      <Route path ="/dash"> <Dash/> </Route>
+        <button> <Link to ="/dash"> Play!</Link> </button>
+        </Switch>
+      </>
     </div>
   );
 }
